a051d462e7441d54d26b8825fc6aca7a15966f2b
Robert DeVore <deviodigital@gmail.com>
2025-02-05 22:31:21 -0500

    :package: NEW: Added WPCom Check to restrict plugin usage on wordpress.com

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e4923e2e154965911cf8330d0f26d0453890dbe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e4923e2e154965911cf8330d0f26d0453890dbe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robertdevore/wpcom-check' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '25eb61d7e0fbd4b2a87a81c3fa7dca722f8d8058',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robertdevore/wpcom-check',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
