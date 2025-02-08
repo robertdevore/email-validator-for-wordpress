feaacbad958aa4c107ca27a743aa5d178e4a2e52
Robert DeVore <deviodigital@gmail.com>
2025-02-08 00:52:21 -0500

    :bug: BUG: Fixed plugin slug in PluginUpdateChecker

--- email-validator-for-wordpress.php
+++ email-validator-for-wordpress.php
@@ -31,7 +31,7 @@ require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/deviodigital/dispensary-age-verification/',
+	'https://github.com/deviodigital/email-validator-for-wordpress/',
 	__FILE__,
 	'email-validator-for-wordpress'
 );
